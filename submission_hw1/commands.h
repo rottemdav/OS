e1062e2abf60c5df97c517ef48908e3b04cbbe26
segevbr <segevbr@gmail.com>
2024-12-17 01:32:37 +0200

    Added files for submission

---
+++ submission_hw1/commands.h
@@ -0,0 +1,100 @@
+#ifndef __COMMANDS_H__
+#define __COMMANDS_H__
+/*=============================================================================
+* includes, defines, usings
+=============================================================================*/
+#include <stdlib.h> //for NULL
+#include <sys/stat.h>
+#include <string.h>
+#include "signals.h"
+#include "jobs.h"
+
+#define MAX_LINE_SIZE 80
+#define MAX_ARGS 20
+#define MAX_COMMANDS 26
+
+/*=============================================================================
+* error definitions
+=============================================================================*/
+enum cmdType {
+	COND_CMD = 0,
+	NOT_COND_CMD = 1,
+	LAST = 2,
+};
+
+enum cmdStatus {
+	COMMAND_SUCCESS = 0,
+	INVALID_COMMAND = 1,
+	MEM_ALLOC_ERR = 2,
+	COMMAND_FAILED = 3,
+	NEWLINE = 4,
+	QUIT_CMD = 5,
+};
+
+/*=============================================================================
+* global functions
+=============================================================================*/
+typedef struct cmd {
+	char* cmd;
+	char* args[MAX_ARGS];
+	char* cmdFull;
+	int numArgs;
+} Command;
+
+typedef struct compCmd {
+	char* line; // String
+	int type; // Type of dependency on the previous function
+} compCmd;
+
+/**
+ * @brief Parses a whole line and return array of commands strings if necessary
+ * 
+ * @param line pointer to user line input
+ * @param commandsArray pointer to array of commands. Will be set as NULL on calling.
+ * @param numCommands number of commands parsed out. input expected to be 0
+ * @return cmdStatus
+ */
+int parseLine(char* line, compCmd** commandsArray, int* numCommands);
+
+/**
+ * @brief Deallocate the memory of the commands array
+ * 
+ * @param commandsArray pointer to commands array
+ * @param count MAX_COMMANDS
+ */
+void freeCommandsArray(compCmd** commandsArray, int count);
+
+int parseCmd(char* line, Command* outCmd);
+
+void freeCommand(Command* cmd);
+
+// Will get a command after parsing and handle according to needs
+int handleCmd(Command* cmd, Job** jobsTable);
+
+int chooseBuiltIn(Command* cmd, Job** jobsTable);
+
+int handleShowPid(Command* cmd);
+
+int handlePwd(Command* cmd);
+
+int handleCd(Command* cmd);
+
+int handleJobs(Command* cmd, Job** jobsTable);
+
+int handleKill(Command* cmd, Job** jobsTable);
+
+int handleFg(Command* cmd, Job** jobsTable);
+
+int handleBg(Command* cmd, Job** jobTable);
+
+int handleQuit(Command* cmd, Job** jobsTable);
+
+int handleDiff(Command* cmd);
+
+int handleExternal(Command* cmd, Job** jobsTable);
+
+bool isBuiltInCmd(Command* cmd);
+
+int maxJobNum(Job** jobsTable);
+
+#endif //__COMMANDS_H__
---
+++ submission_hw1/jobs.c
@@ -0,0 +1,191 @@
+#include "jobs.h"
+#include <stdlib.h> //for NULL
+
+Job** createTable() {
+    // Initializing array of job pointers
+    Job** jobsTable = (Job**)malloc(NUM_JOBS * sizeof(Job*));
+    
+    // Checking memeory allocation
+    if (!jobsTable) return NULL;
+
+    // Set all jobs as free and not stopped
+    for (int i = 0; i < NUM_JOBS; i++){
+        
+        // Allocate memory for each job in array
+        jobsTable[i] = (Job*)malloc(sizeof(Job));
+
+        // Check allocation, if failed at any point, delete the all previous jobs 
+        if (!jobsTable[i]){
+            destroyTable(jobsTable); // deallocate table memory
+            free(jobsTable); // free table pointer
+            jobsTable = NULL;
+            return NULL;
+        }
+        
+        // otherwise, initialize the job
+        jobsTable[i]->jobNum = i + 1;
+        jobsTable[i]->isFree = true;
+        jobsTable[i]->jobPid = -1;
+        jobsTable[i]->startTime = time(NULL); // Sets current time
+        jobsTable[i]->cmdString = NULL; 
+        jobsTable[i]->isStopped = false;
+    }
+    return jobsTable;
+}
+
+/**
+ * @brief updates the job table, deletes a job from the table if terminated or exited
+ * 
+ * @param jobsTable pointer to the jobs table
+ * @return SUCCESS or FAILURE
+ */
+int checkJobs(Job** jobsTable){
+    if (!jobsTable) return ERROR;
+    else{
+        // Iterate all the jobs
+        for (int i = 0; i < NUM_JOBS; i++){
+            if (jobsTable[i]->isFree) continue;
+
+            // Check if current job ended (exited / terminated)
+            int status;
+            pid_t result = waitpid(jobsTable[i]->jobPid, &status, WNOHANG 
+                                                               | WUNTRACED);
+
+            if (result == 0) continue; // process still running
+            else if (result == -1){
+
+                if (errno == ECHILD){ // no such child process
+                    if (deleteJobs(jobsTable[i]->jobNum, jobsTable) == ERROR){
+                        return ERROR;
+                    }
+                } else {
+                    fprintf(stderr, "\n");
+                    perror("smash error: waitpid failed");
+                }
+            }
+            else {
+                // Process state has changed
+                if (WIFEXITED(status) || WIFSIGNALED(status)){ 
+                    // Process has terminated
+                    if (deleteJobs(jobsTable[i]->jobNum, jobsTable) == ERROR){
+                        return ERROR;
+                    }
+                } else if (WIFSTOPPED(status)) {
+                    jobsTable[i]->isStopped = true;
+                }
+            }
+        }
+    }
+    return SUCCESS;
+}
+
+/**
+ * @brief adds a job to the table of jobs given the table has room
+ * 
+ * @param jobsTable pointer to the jobs table
+ * @param jobPid pid of the job given by the system
+ * @param cmd the WHOLE command string given by the user
+ * @return SUCCESS or FAILURE 
+ */
+int addJob(Job** jobsTable, pid_t jobPid, char* cmd){
+    
+    // Check if command or job table are null
+    if (!cmd || !jobsTable) return ERROR;
+    
+    if (checkJobs(jobsTable) == ERROR) return ERROR;
+    
+    int status;
+    pid_t result = waitpid(jobPid, &status, WNOHANG | WUNTRACED);
+    bool isStopped = (result > 0 && WIFSTOPPED(status));
+
+    for (int i=0; i < NUM_JOBS; i++){
+        if (!jobsTable[i]->isFree) continue;
+        else { // Found minimal job id that is free
+            jobsTable[i]->isFree = false;
+            jobsTable[i]->jobPid = jobPid;
+            jobsTable[i]->startTime = time(NULL); 
+            jobsTable[i]->isStopped = isStopped;
+            jobsTable[i]->cmdString = (char*)malloc(strlen(cmd)*sizeof(char)+1);
+            if (!jobsTable[i]->cmdString) return ERROR;
+            strncpy(jobsTable[i]->cmdString, cmd, strlen(cmd) + 1);
+            jobsTable[i]->cmdString[strlen(cmd)] = '\0';
+            
+            break;
+        }
+    }
+   // }
+   
+   return SUCCESS;
+}
+
+/**
+ * @brief prints a single job
+ * 
+ * @param jobsTable pointer to the job table
+ * @param idx job id
+ */
+void printJob(Job** jobsTable, int idx) {
+    if (!jobsTable[idx]->isFree) {
+        time_t elapsedTime = time(NULL) - jobsTable[idx]->startTime;
+
+        if (elapsedTime < 0) elapsedTime = 0;
+
+        printf("[%d] %s: %d %ld secs %s\n" ,
+                    jobsTable[idx]->jobNum,
+                    jobsTable[idx]->cmdString,
+                    jobsTable[idx]->jobPid,
+                    (long)elapsedTime,
+                    ((jobsTable[idx]->isStopped) ?  "(stopped)" : "" ));
+    }
+}
+
+void continueJob(int jobNum, Job** jobsTable) {
+    jobsTable[jobNum - 1]->isStopped = false;
+}
+
+/**
+ * @brief Deletes a job from the job table, if there is one
+ * 
+ * @param jobNum job id given by the addJob() function
+ * @param jobsTable job table with all of the current jobs
+ * @return SUCCESS or FAILURE
+ */
+int deleteJobs(int jobNum, Job** jobsTable) {
+    // If a job is free we shouldn't be able to delete it.
+    if (jobsTable[jobNum - 1]->isFree) return ERROR;
+    
+    jobsTable[jobNum - 1]->isFree=true;
+    jobsTable[jobNum - 1]->jobPid = -1;
+    jobsTable[jobNum - 1]->cmdString = NULL;
+    jobsTable[jobNum - 1]->startTime = time(NULL);
+    jobsTable[jobNum - 1]->isStopped = false;
+    return SUCCESS;
+}
+
+/**
+ * @brief free all the memory allocated for the job table from the inside out,
+ *        first the string (if exists) then the job itself.
+ *        Doesn't free jobsTable because it's an outside pointer.
+ * 
+ * @param jobsTable pointer to the jobs table.
+ */
+void destroyTable(Job** jobsTable){
+    if (jobsTable == NULL) return;
+
+    for (int i = 0; i < NUM_JOBS; i++){
+        if (jobsTable[i] != NULL){
+
+            // deallocating the command string (if exists) and sets to NULL
+            if (jobsTable[i]->cmdString != NULL) {
+                free(jobsTable[i]->cmdString);
+                jobsTable[i]->cmdString = NULL;
+            } 
+            // deallocating the pointer to the job and sets to NULL
+            free(jobsTable[i]);
+            jobsTable[i] = NULL;
+        }
+    }
+}
+
+
+
