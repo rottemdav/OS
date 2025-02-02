e4bc501a91cbafea84440fd610a56dea149588e0
Rottem <rottemdavid@gmail.com>
2025-02-02 22:28:44 +0200

    added customFree, calloc & realloc
    Co-authored-by: Segev Baron <segevbr@gmail.com>

--- MEM_ALL/customAlloctor.c
+++ MEM_ALL/customAlloctor.c
@@ -1,23 +1,45 @@
 #include "customAllocator.h"
 #include <unistd.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 // use of blockList to create the linked list
 // when the program executes there's always a program break
 
 // get the current program break
-void* initial_prog_brk = sbrk(0);
+//void* initial_prog_brk = sbrk(0);
 
-// assign the program break to the block list pointer and initialize the size to 0
-block_list = (blockList*)sbrk(sizeof(blockList));
-if (block_list == SBRK_FAIL) {
-    perror("error"); // need to change to the correct print
+// Create a blockList to store the blocks - set as static object
+static void* initial_prog_brk = NULL;
+static blockList* block_list = NULL;
+
+__attribute__((constructor)) 
+static void initAlloctor(void){
+    // Save the initial program break
+    initial_prog_brk = sbrk(0);
+    block_list = (blockList*)sbrk(sizeof(blockList));
+    if (block_list == SBRK_FAIL) {
+        perror("error"); // need to change to the correct print
+    }
+
+    block_list->block_arr = NULL;
+    block_list->total_size = 0;
+    block_list->allocated_size = 0;
+    block_list->blocks_num = 0;
+    block_list->allocated_blocks = 0;
 }
-block_list->block_arr = NULL;
-block_list->total_size = 0;
-block_list->allocated_size = 0;
-block_list->blocks_num = 0;
-block_list->allocated_blocks = 0;
+
+// // assign the program break to the block list pointer and initialize the size to 0
+// block_list = (blockList*)sbrk(sizeof(blockList));
+// if (block_list == SBRK_FAIL) {
+//     perror("error"); // need to change to the correct print
+// }
+// block_list->block_arr = NULL;
+// block_list->total_size = 0;
+// block_list->allocated_size = 0;
+// block_list->blocks_num = 0;
+// block_list->allocated_blocks = 0;
 
 
 void* customMalloc(size_t size) {
@@ -37,15 +59,16 @@ void* customMalloc(size_t size) {
         size_t size_diff = free_block->size - size; // set the diffrent to default
         Block* match_block = free_block;
         // go through all of the blocks
-        for (int i = 0; i < block_list->blocks_num; i++) {
-            if (block_list->block_arr[i]->free) {
-
-                size_t curr_size_diff = block_list->block_arr[i]->size - size;
+        Block* block_it = block_list->block_arr;
+        while (block_it->next != NULLL) {
+            if (block_it->free) {
+                size_t curr_size_diff = block_it->size - size;
                 if(curr_size_diff < size_diff && curr_size_diff >= size) { // found closer block from the default
-                    match_block = block_list->block_arr[i];
+                    match_block = block_it;
                 } 
             }
-        } // end for
+            block_it = bllock_it->next;
+        }
 
     match_block->free = false;
     block_list->allocated_blocks++;
@@ -57,13 +80,15 @@ void* customMalloc(size_t size) {
         // the block list is empty or any existing block didn't match, so we need to allocate new
         // increment the size of the program break by size bytes
         size_t aligned_size = ALIGN_TO_MULT_OF_4(size)
-        void* new_prog_brk = sbrk(aligned_size);
+        //void* new_prog_brk = sbrk(aligned_size);
         if (new_prog_brk == SBRK_FAIL) {
             perror("error"); // change the to correct printing
         }
 
+        //  (Block*)p + 1 
+
         //creates new block to assign
-        Block* new_block = (Block*)sbrk(sizeof(Block));
+        Block* new_block = (Block*)sbrk(sizeof(Block) + aligned_size);
         if (new_block == SBRK_FAIL) {
             perror ("error"); // need to change to the correct print
             return NULL;
@@ -88,14 +113,198 @@ void* customMalloc(size_t size) {
         block_list->blocks_num++;
         block_list->allocated_blocks++;
 
-        block_list->block_arr = (Block*)new_prog_brk;
-        return (void*)block_list->block_arr;
+        //block_list->block_arr = (Block*)new_prog_brk;
+        return (void*)(new_block + 1);
     }
 }
 
-void customFree(void* ptr);
-void* customCalloc(size_t nmemb, size_t size);
-void* customRealloc(void* ptr, size_t size);
+void customFree(void* ptr){
+    // Check if given pointer is NULL
+    if (ptr == NULL){
+        perror("<free error>: passed null pointer");
+        return;
+    }
+
+    // Set the pointer to the block to free to the start of the header of the block
+    Block* block_to_free = (Block*)ptr - 1;
+
+    // Mark the block as free if not already free and uptade the allocated blocks and size
+    if (!block_to_free->free){
+        block_to_free->free = true;
+        block_list->allocated_blocks--;
+        block_list->allocated_size -= block_to_free->size;
+    }
+
+    // Try to merge blocks in memory if possible
+    Block* block_it = block_list->block_arr;
+    while (block_it != NULL && block_it->next != NULL){
+        // If the current and next blocks are free and adjacent, merge them
+        if (block_it->free && block_it->next->free && areAdjacentBlocks(block_it, block_it->next)){
+            // New size = current size + header + size of next block
+            block_it->size += sizeof(Block) + block_it->next->size;
+            block_it->next = block_it->next->next;
+            block_list->blocks_num--;
+        } else {
+            block_it = block_it->next;
+        }
+    }
+
+    // When arriving to the last block, try to shrink the program break if possible
+    
+    // Get the last block in the blocks list
+    Block* last_block = block_list->block_arr;
+    Block* prev_block = NULL;
+    while (last_block != NULL && last_block->next != NULL){
+        prev_block = last_block;
+        last_block = last_block->next;
+    }
+
+    if (last_block != NULL && last_block->free){
+        size_t size_to_shrink = sizeof(Block) + last_block->size;
+        
+        // Try to shrink the program break, if not possible print error and free all the memory
+        void* result = sbrk(-((intptr_t)size_to_shrink));
+        if (result == SBRK_FAIL){
+            if (errno == ENOMEM){
+                perror("<sbrk erorr>: out of memory");
+
+                // Call function to free all memory blocks and exit 
+                freeAllMemory();
+                exit(1);
+            } 
+        } else {
+            if (prev_block != NULL)
+                prev_block->next = NULL; // Set current last block to NULL
+            else // Reached the first block
+                block_list->block_arr = NULL;
+            
+            // Update the block list's metadata
+            block_list->blocks_num--;
+            block_list->total_size -= size_to_shrink;
+        }
+    }
+}
+   
+
+// Helper function to check if 2 blocks are adjacent to one another
+bool areAdjacentBlocks(Block* block1, Block* block2){
+    return (block1 + block1->size == block2);
+}
+
+// Helper function to free all memory blocks
+void freeAllMemory(){
+    // Set program break to the initial program break
+    brk(initial_prog_brk);
+
+    // Reset the block list metadata
+    block_list->block_arr = NULL;
+    block_list->total_size = 0;
+    block_list->allocated_size = 0;
+    block_list->blocks_num = 0;
+    block_list->allocated_blocks = 0;     
+}
+
+// Allocates memory for array of nmemb elements with size bytes each and set all to 0
+void* customCalloc(size_t nmemb, size_t size){
+    // Check if passed nonpositive value
+    if (nmemb <= 0 || size <= 0){
+        perror("<calloc error>: passed nonpositive value");
+        return NULL;
+    }
+
+    // Calculate the total size of allocation
+    size_t total_size = nmemb * size;
+    
+    void* ptr = customMalloc(total_size);
+    if (ptr == NULL)
+        return NULL;
+    
+    // Set all the allocated memory bytes to 0
+    memset(ptr, 0, total_size);
+
+    return ptr;
+}   
+
+// size < old size
+// (0) / header: sizeof(Block) / data ... / ... / ... size ... / ... / old size 
+
+//changes the size of  the block
+void* customRealloc(void* ptr, size_t size) {
+    if (!ptr) {
+        return customMalloc(size);
+    }
+
+    Block* block = ((Block*)ptr) - 1;
+    size_t old_size = block->size;
+
+    if (size == old_size) {
+        return ptr;
+    }
+
+    if (size < old_size) { //free the excess memory
+        size_t excess_mem = old_size - size;
+        // call customFree for the ptr offset?
+        
+        // If we have enough excess memory, we create a new block to split the old one
+        if (excess_mem >= (sizeof(Block) + 1)){
+            // Set the size of the current block to the new size
+            block->size = size;
+
+            // Set the new block to the excess memory
+            Block* new_free_block = (Block*)((char*)block + sizeof(Block) + size);
+            new_free_block->size = excess_mem - sizeof(Block);
+            new_free_block->free = true;
+            new_free_block->next = block->next;
+            block->next = new_free_block;
+
+            // Update the block list's metadata
+            block_list->blocks_num++;
+            block_list->allocated_size -= new_free_block->size;
+
+            return ptr;
+        } else { // In case there is not enough excess memory
+            // Allocate new memory block with the new size
+            void* new_ptr = customMalloc(size);
+            
+            // Check allocation success
+            if (new_ptr == NULL)
+                return NULL;
+
+            // Copy the data from the old block to the new one and delete the old block
+            memcpy(new_ptr, ptr, size);
+            customFree(ptr);
+            return new_ptr;
+        }
+    } else { // size > old_size
+        // Allocate new memory block with the new size
+        void* new_ptr = customMalloc(size);
+
+        // Check allocation success
+        if (new_ptr == NULL)
+            return NULL;
+        
+        // Copy the data from the old block to the new one and delete the old block
+        memcpy(new_ptr, ptr, old_size);
+        customFree(ptr);
+        return new_ptr;
+    }
+        
+    //     if (!customFree(ptr + sizeof(Block) + )) {
+    //          // if free failed
+    //         Block* new_block = (Block*)customMalloc(size);
+    //         memcpy((new_block+1), (Block*)(ptr+1), size);
+    //         customFree(ptr);
+    //         block->size = size;
+    //         return (void*)(new_block + 1);
+    //     }
+    // } else { // size > old_size
+    //     Block* new_block = (Block*)customMalloc(size);
+    //     memcpy((new_block + 1), (Block*)(ptr+1),size);
+    //     custromFree(ptr);
+    //     return (void*)(new_block + 1);
+    // }
+   
+}
 
 Block* findFirstFreeBlock (size_t req_size) {
     Block* block_it = block_list->block_arr;
